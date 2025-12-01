49cf939f2aee3734c0aeb8063ab6c6bf60e51d68
IsharaNawa <ikos0266@uni.sydney.edu.au>
2025-12-01 21:17:56 +1100

    Add Rocket90MHZ device tree and update configuration files for Genesys2

--- setup_genesys2.sh
+++ setup_genesys2.sh
@@ -7,5 +7,6 @@ UBOOT_DIR=~/Research/repos/u-boot
 cd $UBOOT_DIR
 
 make distclean
-make CROSS_COMPILE=riscv64-linux-gnu- genesys2_defconfig
+# make CROSS_COMPILE=riscv64-linux-gnu- genesys2_defconfig
+make CROSS_COMPILE=riscv64-linux-gnu- Rocket90MHZ_defconfig
 make CROSS_COMPILE=riscv64-linux-gnu- -j16
